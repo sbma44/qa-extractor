6deda7a73d9d3890893d6e156e779a599c7f9589
ingalls <nicholas.ingalls@gmail.com>
2016-09-08 10:50:35 -0400

    Add test file

---
+++ test/map.test.js
@@ -0,0 +1,17 @@
+var map = require('../lib/map');
+var test = require('tape');
+var path = require('path');
+var fs = require('fs');
+var _ = require('lodash');
+var os = require('os');
+
+var tmpOutput = os.tmpdir() + '/output.geojson';
+
+test('map - basic place filter', function(t) {
+    map({
+        input: __dirname + '/sg.mbtiles',
+        output: tmpOutput
+    });
+
+    t.end();
+});
