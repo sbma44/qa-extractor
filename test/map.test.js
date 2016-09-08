8d3d4aac0f2287707a65d810c7223eb238d946aa
ingalls <nicholas.ingalls@gmail.com>
2016-09-08 16:41:01 -0400

    Add some more test queries

--- test/map.test.js
+++ test/map.test.js
@@ -7,12 +7,49 @@ var os = require('os');
 
 var tmpOutput = os.tmpdir() + '/output.geojson';
 
-test('map - basic place filter', function(t) {
+function count(t, ct, cb) {
+    var i;
+    var count = 0;
+    require('fs').createReadStream(tmpOutput)
+    .on('data', function(chunk) {
+        for (i=0; i < chunk.length; ++i) if (chunk[i] == 10) count++; })
+    .on('end', function() {
+        t.equals(count, ct);
+
+        if (cb) return cb();
+        else t.end();
+    });
+}
+
+test('map - building', function(t) {
     map({
         input: __dirname + '/sg.mbtiles',
         output: tmpOutput,
         selector: 'building'
+    }, function(parsed) {
+        t.deepEquals(parsed, { '+building': {} });
+        count(t, 50260);
     });
+});
 
-    t.end();
+test('map - building addr', function(t) {
+    map({
+        input: __dirname + '/sg.mbtiles',
+        output: tmpOutput,
+        selector: 'building addr'
+    }, function(parsed) {
+        t.deepEquals(parsed, {"+building":{"+addr":{}}});
+        count(t, 18746);
+    });
+});
+
+test('map - building "addr:city"', function(t) {
+    map({
+        input: __dirname + '/sg.mbtiles',
+        output: tmpOutput,
+        selector: 'building "addr:city"'
+    }, function(parsed) {
+        t.deepEquals(parsed, { '+building': { '+addr:city': { '+': {} } } });
+        count(t, 8856);
+    });
 });
